44681c1c5f4099e93397355de888f334ebcac901
JeffreyLantinga <jeffreylantinga@hotmail.com>
2017-05-31 09:21:05 +0200

    test for git

--- MyApplication/app/src/main/java/com/lantinga/cv_application/SubMenuScreen.java
+++ MyApplication/app/src/main/java/com/lantinga/cv_application/SubMenuScreen.java
@@ -2,6 +2,7 @@ package com.lantinga.cv_application;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by Jeffrey on 16-5-2017.
@@ -16,7 +17,8 @@ public class SubMenuScreen extends AppCompatActivity {
 
         SubPages page = (SubPages) getIntent().getSerializableExtra("SUB_PAGE");
 
-
+        ImageView image = (ImageView) findViewById(R.id.picOfJeff);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.a_pic_of_me));
 
 
     }
